4d9fe4efb82745340a51b11521b5b03fcc1797e0
BlackReaper5 <escanor666111@gmail.com>
2019-05-12 11:22:08 +0200

    Added solution

---
+++ Filtering/Assignment_1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageUtilities;
+
+
+namespace Filtering
+{
+    public class Assignment_1
+    {
+        public static Color[,] RGBtoGrayScale(string path)
+        {
+            Bitmap inputBitmap = new Bitmap(path);
+
+            Color[,] inputImage = ImageViewer.Bitmap2colorm(inputBitmap);
+            for (int i = 0; i < inputBitmap.Width; i++)
+            {
+                for (int j = 0; j < inputBitmap.Height; j++)
+                {
+                    Color coloredpixels = inputBitmap.GetPixel(i, j); 
+                    byte r = coloredpixels.R;
+                    byte g = coloredpixels.G;
+                    byte b = coloredpixels.B;
+                    int avg = (r + g + b) / 3;
+                    Color avgColor = Color.FromArgb(avg, avg, avg);
+                    inputBitmap.SetPixel(i, j, avgColor); 
+
+                    inputImage[i, j] = inputBitmap.GetPixel(i, j);
+                }
+            }
+            return inputImage;
+
+        }
+
+        public static Color[,] ImagetoRGB(string path) // TODO: Think about whether this implementation of RGB encoding is correct
+            
+        {
+            Bitmap inputBitmap = new Bitmap(path);
+
+            Color[,] inputImage = ImageViewer.Bitmap2colorm(inputBitmap);
+            for (int i = 0; i < inputBitmap.Width; i++)
+            {
+                for (int j = 0; j < inputBitmap.Height; j++)
+                {
+                    Color coloredpixels = inputBitmap.GetPixel(i, j); 
+                    byte r = coloredpixels.R;
+                    byte g = coloredpixels.G;
+                    byte b = coloredpixels.B;
+
+                    Color rgb = Color.FromArgb(r, g, b);
+                    inputBitmap.SetPixel(i, j, rgb); 
+
+                    inputImage[i, j] = inputBitmap.GetPixel(i, j);
+                }
+            }
+            return inputImage;
+
+        }
+    }
+}
+
---
+++ Template/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using ImageUtilities;
+using Filtering;
+
+namespace Template
+{
+    class Program
+    {
+        public static void RGBGray()
+        {
+            //string path = args[0]; // right click template -> properties -> Debug -> command line arguments -> "../../../images/tulips.png"
+            string path = @"../../../images/forest.jpg";
+            string pathout = path.Substring(0, path.LastIndexOf(".")) + "-out" + path.Substring(path.LastIndexOf("."), path.Length - path.LastIndexOf("."));
+            Color[,] im = ImageViewer.LoadImage(path);
+            Color[,] GrayRGB = Assignment_1.RGBtoGrayScale(path); // RGBtoGrayScale
+            ImageViewer.DrawImage(GrayRGB);
+            ImageViewer.DrawImagePair(GrayRGB, im);
+            ImageViewer.SaveImage(im, pathout);
+            
+        }
+
+        public static void RGB()
+        {
+            //string path = args[0]; // right click template -> properties -> Debug -> command line arguments -> "../../../images/tulips.png"
+            string path = @"../../../images/forest.jpg";
+            string pathout = path.Substring(0, path.LastIndexOf(".")) + "-out" + path.Substring(path.LastIndexOf("."), path.Length - path.LastIndexOf("."));
+            Color[,] im = ImageViewer.LoadImage(path);
+            Color[,] RGBImage = Assignment_1.ImagetoRGB(path); // RGBtoGrayScale
+            ImageViewer.DrawImage(RGBImage);
+            ImageViewer.DrawImagePair(RGBImage, im);
+            ImageViewer.SaveImage(im, pathout);
+
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+           
+            //RGBGray(); ////Convert RGB image to grayscale
+            RGB(); 
+            ////string path = args[0]; // right click template -> properties -> Debug -> command line arguments -> "../../../images/tulips.png"
+            //string path = @"../../../images/forest.jpg";
+            //string pathout = path.Substring(0, path.LastIndexOf(".")) + "-out" + path.Substring(path.LastIndexOf("."), path.Length - path.LastIndexOf("."));
+            //Color[,] im = ImageViewer.LoadImage(path);
+            //Color [,] GrayRGB = Assignment_1.RGBtoGrayScale(path); // RGBtoGrayScale
+            //ImageViewer.DrawImage(GrayRGB);
+            //ImageViewer.DrawImagePair(GrayRGB, im);
+            //ImageViewer.SaveImage(im, pathout);
+        }
+    }
+}
